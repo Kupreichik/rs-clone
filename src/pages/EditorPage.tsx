43f7fe261836998903256f71dbc66d3168645378
Andrey Boytsov <po4ta_notice@mail.ru>
2023-02-03 12:29:23 +0300

    feat: add result html view in iframe

--- src/pages/EditorPage.tsx
+++ src/pages/EditorPage.tsx
@@ -1,6 +1,6 @@
 import './EditorsPage.scss';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Editor from '../components/Editor/Editor';
 
@@ -9,11 +9,36 @@ export const EditorPage = () => {
   const [css, setCss] = useState('');
   const [js, setJS] = useState('');
 
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>
+    `);
+    }, 250);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [html, css, js]);
+
+  console.log('JSON.stringify(srcDoc)-->', JSON.stringify(srcDoc));
+
   return (
-    <div className="editors-container">
-      <Editor language="xml" displayName="HTML" onChange={setHtml} value={html} />
-      <Editor language="css" displayName="CSS" onChange={setCss} value={css} />
-      <Editor language="javascript" displayName="JAVASCRIPT" onChange={setJS} value={js} />
-    </div>
+    <>
+      <div className="editors-container">
+        <Editor language="xml" displayName="HTML" onChange={setHtml} value={html} />
+        <Editor language="css" displayName="CSS" onChange={setCss} value={css} />
+        <Editor language="javascript" displayName="JAVASCRIPT" onChange={setJS} value={js} />
+      </div>
+      <div className="results-container">
+        <iframe srcDoc={srcDoc} title="results" sandbox="allow-scripts" width="100%" height="100%" />
+      </div>
+    </>
   );
 };
