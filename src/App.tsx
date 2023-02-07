0f56aac3d1bad49ae7e31712ab4c54e7033178b6
Sergey <fserega2014@yandex.ru>
2023-02-07 13:02:52 +0300

    feat: add header and desktop style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { Layout } from './components/Layout';
+import { Account } from './pages/Account';
 import { Homepage } from './pages/Homepage';
+import { Login } from './pages/Login';
 import { Notefoundpage } from './pages/Notefoundpage';
 import { Workspace } from './pages/Workspace';
 
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Homepage />} />
           <Route path="workspace" element={<Workspace />} />
+          <Route path="login" element={<Login />} />
+          <Route path="account" element={<Account />} />
           <Route path="*" element={<Notefoundpage />} />
         </Route>
       </Routes>
