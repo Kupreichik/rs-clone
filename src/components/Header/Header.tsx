f057413fd66847bc15bfdd7d2a3f38c2bb4959bc
Andrey Boytsov <po4ta_notice@mail.ru>
2023-02-12 14:46:35 +0300

    feat: for header add condition to render search bar and pen info

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import cn from 'classnames';
 import { useState } from 'react';
 import { MdMenu, MdMenuOpen } from 'react-icons/md';
 import Media from 'react-media';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 import { ReactComponent as LogoDesktop } from '../../assets/svg/logoDesktop.svg';
 import { ReactComponent as LogoMobile } from '../../assets/svg/LogoMobile.svg';
@@ -15,6 +15,8 @@ const setLoginButton = ({ isActive }: { isActive: boolean }) => ({ display: isAc
 
 export const Header = () => {
   const [burger, setBurger] = useState(false);
+  const locationRouter = useLocation();
+
   return (
     <header className={styles.header}>
       <div className="container">
@@ -25,13 +27,15 @@ export const Header = () => {
           <div onClick={() => setBurger(!burger)} className={styles.header__burger}>
             {burger ? <MdMenuOpen size={22} /> : <MdMenu size={22} />}
           </div>
-          <PenInfo />
-          <form action="" className={styles.header__form}>
-            <label className={styles['header__form-label']}>
-              <Magnifier className={styles['header__form-icon']} />
-              <input className={styles.header__input} type="text" placeholder="Search CodePen..." />
-            </label>
-          </form>
+          {locationRouter.pathname === '/editor' && <PenInfo />}
+          {locationRouter.pathname !== '/editor' && (
+            <form action="" className={styles.header__form}>
+              <label className={styles['header__form-label']}>
+                <Magnifier className={styles['header__form-icon']} />
+                <input className={styles.header__input} type="text" placeholder="Search CodePen..." />
+              </label>
+            </form>
+          )}
           <div className={styles.header__buttons}>
             <NavLink className={cn(styles.header__button, 'button')} style={setLoginButton} to="/account">
               Sign Up
