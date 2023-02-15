6cc974c68e3e1edafc37bf1302597f36068d6f21
Sergey <fserega2014@yandex.ru>
2023-02-16 00:42:36 +0300

    refactor: change alert in Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,6 @@
-import { Button, TextField } from '@mui/material';
+import { Button, Snackbar, TextField } from '@mui/material';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import { forwardRef, SyntheticEvent, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useSelector } from 'react-redux';
 import { Link, Navigate } from 'react-router-dom';
@@ -7,7 +9,12 @@ import { fetchAuth, selectIsAuth, UserData } from '../redux/slices/auth';
 import { useAppDispatch } from '../redux/store';
 import styles from './auth.module.scss';
 
+const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 export const Login = () => {
+  const [open, setOpen] = useState(false);
   const isAuth = useSelector(selectIsAuth);
 
   const dispatch = useAppDispatch();
@@ -28,13 +35,21 @@ export const Login = () => {
     const data = await dispatch(fetchAuth(values));
 
     if (!data.payload) {
-      return alert('Failed to log in');
+      setOpen(true);
     }
   };
 
   if (isAuth) {
     return <Navigate to="/" />;
   }
+
+  const handleClose = (event?: SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   return (
     <section className={styles.auth}>
@@ -80,6 +95,16 @@ export const Login = () => {
           >
             Log In with GitHub
           </Link>
+          <Snackbar
+            open={open}
+            autoHideDuration={3000}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          >
+            <Alert onClose={handleClose} severity="error" sx={{ width: 482 }}>
+              Failed to log in!
+            </Alert>
+          </Snackbar>
         </div>
       </div>
     </section>
