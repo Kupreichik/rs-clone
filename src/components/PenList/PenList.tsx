c0e40660204656ed65d9271c43e7f4d36a1203a1
Andrey Boytsov <po4ta_notice@mail.ru>
2023-02-17 00:16:19 +0300

    fix: add preloader in PenList

--- src/components/PenList/PenList.tsx
+++ src/components/PenList/PenList.tsx
@@ -3,13 +3,14 @@ import { useSelector } from 'react-redux';
 import styles from '../../pages/home.module.scss';
 import { RootState } from '../../redux/store';
 import { PenItem } from '../PenItem/PenItem';
+import Preloader from '../Preloader/Preloader';
 
 export const PenList = () => {
   const pens = useSelector((state: RootState) => state.pens.pens);
   const status = useSelector((state: RootState) => state.pens.status);
 
   return status !== 'loaded' ? (
-    <div style={{ textAlign: 'center' }}>Loading...</div>
+    <Preloader />
   ) : (
     <div className={styles.home__items}>
       {pens.map((penData) => (
