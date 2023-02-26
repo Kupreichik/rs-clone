aac415b17d77528711e5754358fe49950225f94e
Sergey <fserega2014@yandex.ru>
2023-02-26 15:29:06 +0300

    feat: add mobile adaptive

--- src/components/PenItem/PenItem.tsx
+++ src/components/PenItem/PenItem.tsx
@@ -38,48 +38,46 @@ export const PenItem = (data: IPenData) => {
 
   return (
     <div className={styles.home__item}>
-      <div className={styles['home__item-content']}>
-        <div className={styles['home__item-background']}></div>
-        <div className={styles['home__item-iframe-wrapper']}>
-          <iframe
-            style={{ border: 0 }}
-            srcDoc={srcDoc}
-            title="results"
-            sandbox="allow-scripts"
-            width="100%"
-            height="100%"
-          />
-          <Link onClick={onLink} className={styles['home__item-link']} to={`/editor/${data._id}`}>
-            <span hidden>Open Editor</span>
-          </Link>
+      <div className={styles['home__item-background']}></div>
+      <div className={styles['home__item-iframe-wrapper']}>
+        <iframe
+          style={{ border: 0 }}
+          srcDoc={srcDoc}
+          title="results"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"
+        />
+        <Link onClick={onLink} className={styles['home__item-link']} to={`/editor/${data._id}`}>
+          <span hidden>Open Editor</span>
+        </Link>
+      </div>
+      <div className={styles['home__item-info']}>
+        <img className={styles['home__item-avatar']} src={data.user.avatar} alt="avatar" />
+        <div className={styles['home__item-description']}>
+          <h4 className={styles['home__item-title']}>{data.title}</h4>
+          <p className={styles['home__item-author']}>{data.user.name}</p>
         </div>
-        <div className={styles['home__item-info']}>
-          <img className={styles['home__item-avatar']} src={data.user.avatar} alt="avatar" />
-          <div className={styles['home__item-description']}>
-            <h4 className={styles['home__item-title']}>{data.title}</h4>
-            <p className={styles['home__item-author']}>{data.user.name}</p>
-          </div>
-          <div
-            className={styles['home__item-like']}
-            onMouseEnter={() => setHeartIcon(true)}
-            onMouseLeave={() => setHeartIcon(false)}
-          >
-            {heartIcon ? (
-              <IoMdHeart className={styles['home__item-icon']} />
-            ) : (
-              <IoMdHeartEmpty className={styles['home__item-icon']} />
-            )}
-          </div>
+        <div
+          className={styles['home__item-like']}
+          onMouseEnter={() => setHeartIcon(true)}
+          onMouseLeave={() => setHeartIcon(false)}
+        >
+          {heartIcon ? (
+            <IoMdHeart className={styles['home__item-icon']} />
+          ) : (
+            <IoMdHeartEmpty className={styles['home__item-icon']} />
+          )}
         </div>
-        <div className={styles['home__item-stats']}>
-          <div className={styles['home__item-stats-content']}>
-            <IoMdHeart />
-            <span>{data.likesCount}</span>
-          </div>
-          <div className={styles['home__item-stats-content']}>
-            <FaEye />
-            <span>{data.viewsCount}</span>
-          </div>
+      </div>
+      <div className={styles['home__item-stats']}>
+        <div className={styles['home__item-stats-content']}>
+          <IoMdHeart />
+          <span>{data.likesCount}</span>
+        </div>
+        <div className={styles['home__item-stats-content']}>
+          <FaEye />
+          <span>{data.viewsCount}</span>
         </div>
       </div>
     </div>
